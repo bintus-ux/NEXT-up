cd783a35af9a1edb860970d9c5f2ce630eb42ad7
Ezerike Caleb Chikere <cezerike@gmail.com>
2024-01-18 12:51:50 +0100

    'update: tailwind styling and tsx setup'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 "use client";
 import { useState, FormEvent } from "react"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 const [inputVal, setInputVal] = useState("");
+const {push} = useRouter()
 
 const handleSubmit = (event: FormEvent) => {
   event.preventDefault();
+  push(`/prediction/${inputVal}`)
 }
 
   return (
-   <div>
-    <div>
-      <h1>Enter your name</h1>
+   <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="p-4 shadow-md bg-white rounded-md">
+      <h1 className="text-2xl font-semibold mb-4 text-black">Enter your name</h1>
     </div>
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Type your name..." onChange={(e) => {setInputVal(e.target.value)}} value={inputVal} />
-      <button type="submit">Predict data</button>
+    <form onSubmit={handleSubmit} className="space-y-3">
+      <input className="w-full p-2 border border-gray-300 rounded text-black" type="text" placeholder="Type your name..." onChange={(e) => {setInputVal(e.target.value)}} value={inputVal} />
+      <button className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white" type="submit">Submit</button>
     </form>
    </div>
   )
